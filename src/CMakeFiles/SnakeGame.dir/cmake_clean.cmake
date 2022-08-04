ee381c2a1320df7f521941ca9af694ed24165dc2
laldoski <lava.aldoski2@gmail.com>
2022-08-03 20:57:27 -0400

    first commit

---
+++ src/CMakeFiles/SnakeGame.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeGame.dir/main.cpp.o"
+  "CMakeFiles/SnakeGame.dir/game.cpp.o"
+  "CMakeFiles/SnakeGame.dir/controller.cpp.o"
+  "CMakeFiles/SnakeGame.dir/renderer.cpp.o"
+  "CMakeFiles/SnakeGame.dir/snake.cpp.o"
+  "SnakeGame.pdb"
+  "SnakeGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
